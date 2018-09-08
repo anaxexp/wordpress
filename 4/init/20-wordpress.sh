f2b7fe877653ff1b2841618154fc505303b5c285
Christopher Wells <cwells@anaxexp.io>
2018-09-08 13:37:55 -0700

    update: fixes

--- 4/init/20-wordpress.sh
+++ 4/init/20-wordpress.sh
@@ -9,3 +9,17 @@ fi
 if [[ -z "${ANAXEXP_APP_NAME}" ]]; then
     make init -f /usr/local/bin/actions.mk
 fi
+
+_gotpl() {
+    if [[ -f "/etc/gotpl/$1" ]]; then
+        gotpl "/etc/gotpl/$1" > "$2"
+    fi
+}
+
+# Remove generic environment config from anaxexp/php
+rm -f "${CONF_DIR}/anaxexp.settings.php"
+
+if [[ -n "${ANAXEXP_APP_NAME}" && -n "${WP_VERSION}" ]]; then
+    _gotpl "anaxexp.wp${WP_VERSION}-config.php.tmpl" "${CONF_DIR}/anaxexp.wp-config.php"
+    _gotpl "wp-config.php.tmpl" "${CONF_DIR}/wp-config.php"
+fi
